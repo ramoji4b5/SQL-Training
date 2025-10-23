6bda62a2f61fee8bb8e2fe9c3febd7e794b6d6af
DuppalaDeepika <137288975+DuppalaDeepika@users.noreply.github.com>
2025-10-23 09:47:24 +0530

    sub_where.sql

--- query11.sql
+++ query11.sql
@@ -1,3 +1,5 @@
+subquery in where clause
+
 SELECT first_name,last_name FROM employees
 WHERE job_id IN ( 
 	SELECT job_id FROM jobs WHERE job_title LIKE '%Sales%');
@@ -9,3 +11,4 @@ WHERE job_id IN (
   ROUND(AVG(department_salary), 0) average_department_salary
 FROM (SELECT department_id, SUM(salary) department_salary
     FROM employees GROUP BY department_id );
+
